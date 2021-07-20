e33a12b36c641f2c2990f5d6e928774c5f5be59e
Ankit Mishra <ankitmishra723@gmail.com>
2021-07-20 21:15:59 +0530

    handling exception

--- threading_main.py
+++ threading_main.py
@@ -1,25 +1,31 @@
 from interface_manager_v3 import *
 import threading
 
+while True:
+    thread = []
 
-thread = []
+    try:
+        for e in all_camera_data.keys():
+            thread.append(threading.Thread(target=main_program, args=(e, all_camera_data[e])))
+            thread[-1].start()
+            time.sleep(2)
 
-#'''
-for e in all_camera_data.keys():
-    thread.append(threading.Thread(target=main_program, args=(e, all_camera_data[e])))
-    thread[-1].start()
-    time.sleep(2)
+        for item in thread:
+            item.join()
 
-for item in thread:
-    item.join()
-# '''
+    except:
+        print('Inside Threading Except')
+        all_camera_data = []
+        VLC_PLAYER_OBJECT = {}
+        gc.collect()
 
 
-# --------------------- FOR TESTING ONLY ------------
 
-"""
-if __name__ == '__main__':
-    e = '5'
-    main_program(e, all_camera_data[e])
+    # --------------------- FOR TESTING ONLY ------------
 
-#"""
+    """
+    if __name__ == '__main__':
+        e = '5'
+        main_program(e, all_camera_data[e])
+    
+    #"""
